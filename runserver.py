5e9b144e226373386d49ba396816a3d1d35f1788
Snow Leopard <myfly245@gmail.com>
2022-04-24 19:11:51 +0300

    0.1.12.4

--- runserver.py
+++ runserver.py
@@ -3,8 +3,10 @@ This script runs the Web_IDE application using a development server.
 """
 
 from os import environ
+import os
 from random import randint
 from violet_main import app
+from waitress import serve
 
 
 if __name__ == '__main__':
@@ -12,7 +14,9 @@ if __name__ == '__main__':
     # PORT = randint(1000, 9999)
     PORT = 5000
     FLASK_ENV = "development"
-    app.run(HOST, PORT, debug=True)
+    # app.run(HOST, PORT, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    serve(app, host='0.0.0.0', port=port)
         
 
    
