5a7622187c5ad59c040feba6abefae26cfa2530e
Snow Leopard <myfly245@gmail.com>
2022-04-24 18:59:07 +0300

    0.1.12.3

--- runserver.py
+++ runserver.py
@@ -7,12 +7,12 @@ from random import randint
 from violet_main import app
 
 
-# if __name__ == '__main__':
-HOST = environ.get('SERVER_HOST', 'localhost')
-# PORT = randint(1000, 9999)
-PORT = 5000
-FLASK_ENV = "development"
-app.run(HOST, PORT, debug=True)
+if __name__ == '__main__':
+    HOST = environ.get('SERVER_HOST', 'localhost')
+    # PORT = randint(1000, 9999)
+    PORT = 5000
+    FLASK_ENV = "development"
+    app.run(HOST, PORT, debug=True)
         
 
    
